9b16fd7dca399ef6d1911b7baf8fe627bbd3b15d
Jair Enrique Gómez Rodríguez <33232689+jairGomezRod@users.noreply.github.com>
2022-12-15 21:27:19 -0500

    :tada: Create project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bunsan contable app
+      </header>
+    </div>
+  );
+}
+
+export default App;
