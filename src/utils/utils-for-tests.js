41a59f299a3a7700b752bc741ad9a913c0047fe9
Jair Enrique Gómez Rodríguez <33232689+jairGomezRod@users.noreply.github.com>
2022-12-19 00:00:24 -0500

    :sparkles: Util for redux tests

---
+++ src/utils/utils-for-tests.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from 'react-redux';
+import { userSlice } from "../redux/states/user.state";
+
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: { userReducers: userSlice.reducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
