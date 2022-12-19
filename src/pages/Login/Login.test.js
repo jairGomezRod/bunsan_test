0c6b93182468e22425ab7189c204fb4f9110cc4f
Jair Enrique Gómez Rodríguez <33232689+jairGomezRod@users.noreply.github.com>
2022-12-19 00:02:52 -0500

    :white_check_mark: Add login test

--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -1,9 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import Login from './index';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { renderWithProviders } from "../../utils/utils-for-tests";
 
 test('renders login view', () => {
-  render(
+
+  renderWithProviders(
     <Router>
       <Login />
     </Router>
@@ -13,11 +16,54 @@ test('renders login view', () => {
 });
 
 test('render form', () => {
-  render(
+  renderWithProviders(
     <Router>
       <Login />
     </Router>
   );
   const signInButton = screen.getByRole('button')
   expect(signInButton).toBeInTheDocument();
+});
+
+test('Show error on bad login', async () => {
+  renderWithProviders(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const emailInput = screen.getByPlaceholderText(/Email Address/i);
+  const passwordInput = screen.getByPlaceholderText(/Password/i);
+  const signInButton = screen.getByRole('button')
+
+  fireEvent.change(emailInput, {
+    target: {value: 'test@test.com'},
+  });
+
+  fireEvent.change(passwordInput, {
+    target: {value: 'jh0nD0eBunsan'},
+  })
+  await userEvent.click(signInButton);
+  
+  expect(screen.getByTestId("error-message")).toBeInTheDocument();
+});
+
+test('Show disabled button on bad format', async () => {
+  renderWithProviders(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const emailInput = screen.getByPlaceholderText(/Email Address/i);
+  const passwordInput = screen.getByPlaceholderText(/Password/i);
+  const signInButton = screen.getByRole('button')
+
+  fireEvent.change(emailInput, {
+    target: {value: 'test@test.com'},
+  });
+
+  fireEvent.change(passwordInput, {
+    target: {value: '12345'},
+  })
+  
+  expect(signInButton).toBeDisabled();
 });
